eafb1395024c86dc2a8f359b013f396b2b42a6c2
iaman8055 <iamanchauhan93@gmail.com>
2024-01-20 03:11:43 +0530

    Utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        catch((err)=>next(err))
+    
+    }
+}
+
+export {asyncHandler}
+
+
+/* async await */
+// const asynHnadler=(fn)=>async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }catch (error) {
+//         res.status(err.code||400).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+
+// }
